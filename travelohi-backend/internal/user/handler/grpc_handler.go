30e5e31fdbcb262cb6369c3a4fac83b04c783b73
ShinyWise <franswinata06@gmail.com>
2026-05-11 13:55:57 +0700

    fix(user.handler): complete basic registration logic and andd missing validation

--- travelohi-backend/internal/user/handler/grpc_handler.go
+++ travelohi-backend/internal/user/handler/grpc_handler.go
@@ -47,3 +47,48 @@ func (h *UserGrpcHandler) GetUser(ctx context.Context, req *userpb.GetUserReques
 }
 
 // wip: bikin sisa CRUD buat user
+
+func (h *UserGrpcHandler) CreateUser(ctx context.Context, req *userpb.CreateUserRequest) (*userpb.UserResponse, error) {
+	// 1. input validation
+	if req.GetEmail() == "" {
+		return nil, errors.New("email is required")
+	}
+	if req.GetFirstName() == "" || req.GetLastName() == "" {
+		return nil, errors.New("first name and last name are required")
+	}
+	if req.GetGender() == "" {
+		return nil, errors.New("gender is required")
+	}
+	if req.GetDob() == "" {
+		return nil, errors.New("date of birth is required")
+	}
+
+	// 2. map proto DTO ke entity domain-> biar bisa dikasih ke usecase -> repo
+	domainUser := &user.User{
+		Email:     req.GetEmail(),
+		FirstName: req.GetFirstName(),
+		LastName:  req.GetLastName(),
+		Gender:    req.GetGender(),
+		DOB:       req.GetDob(),
+	}
+
+	// 3. panggil usecase
+	createdUser, err := h.userUsecase.CreateUser(ctx, domainUser)
+	if err != nil {
+		return nil, err
+	}
+
+	// 4. balikin lagi ke bentuk proto response
+		User: &userpb.User{
+			Id:                createdUser.ID,
+			Email:             createdUser.Email,
+			FirstName:         createdUser.FirstName,
+			LastName:          createdUser.LastName,
+			Gender:            createdUser.Gender,
+			Dob:               createdUser.DOB,
+			ProfilePictureUrl: createdUser.ProfilePictureURL,
+			IsActive:          createdUser.IsActive,
+			IsBanned:          createdUser.IsBanned,
+		},
+	}, nil
+}
