e86b75fbc29d0213d66d0509819ae931398e1cb3
ShinyWise <franswinata06@gmail.com>
2026-05-12 16:40:07 +0700

    feat(auth): implement Registration usecase

---
+++ travelohi-backend/internal/auth/usecase/usecase.go
@@ -0,0 +1,121 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+
+	"github.com/travelohi/backend/internal/auth"
+	"github.com/travelohi/backend/pkg/hash"
+	"github.com/travelohi/backend/pkg/id"
+	"github.com/travelohi/backend/pkg/token"
+	accountpb "github.com/travelohi/backend/proto/account/v1"
+)
+
+type authUseCase struct {
+	repo           auth.AuthRepository
+	cache          auth.CacheRepository
+	accountService accountpb.AccountServiceClient
+	hasher         hash.PasswordHasher
+	idGen          id.Generator
+	tokenMaker     token.Maker
+}
+
+func NewAuthUseCase(
+	repo auth.AuthRepository,
+	cache auth.CacheRepository,
+	accountService accountpb.AccountServiceClient,
+	hasher hash.PasswordHasher,
+	idGen id.Generator,
+	tokenMaker token.Maker,
+) auth.AuthUseCase {
+	return &authUseCase{
+		repo:           repo,
+		cache:          cache,
+		accountService: accountService,
+		hasher:         hasher,
+		idGen:          idGen,
+		tokenMaker:     tokenMaker,
+	}
+}
+
+var _ auth.AuthUseCase = (*authUseCase)(nil)
+
+func (uc *authUseCase) Register(ctx context.Context, req *auth.RegisterData) (*auth.AuthResult, error) {
+	if req.Password != req.ConfirmPassword {
+		return nil, errors.New("Password do not match")
+	}
+
+	_, err := uc.repo.GetByEmail(ctx, req.Email)
+	if err == nil {
+		return nil, errors.New("This email already registered!")
+	} else if !errors.Is(err, auth.ErrInvalidCreds) {
+		return nil, auth.ErrInternal
+	}
+
+	// Hasher here
+	hashedPassword, err := uc.hasher.Hash(req.Password)
+
+	if err != nil {
+		return nil, auth.ErrHashing
+	}
+
+	hashedSecurityAnswer, err := uc.hasher.Hash(req.SecurityAnswer)
+	if err != nil {
+		return nil, auth.ErrHashing
+	}
+
+	// generate id buat account baru
+	newUserID := uc.idGen.Generate()
+
+	vaultRecord := &auth.Auth{
+		ID:                 newUserID,
+		Email:              req.Email,
+		PasswordHash:       hashedPassword,
+		SecurityQuestionID: req.SecurityQuestionID,
+		SecurityAnswerHash: hashedSecurityAnswer,
+		IsBanned:           false,
+	}
+	// masukin ke repo
+	if err := uc.repo.Create(ctx, vaultRecord); err != nil {
+		return nil, auth.ErrInternal
+	}
+
+	// create account, panggil accountServices
+	_, err = uc.accountService.InitProfile(ctx, &accountpb.InitProfileRequest{
+		Id:                   newUserID,
+		Email:                req.Email,
+		FirstName:            req.FirstName,
+		LastName:             req.LastName,
+		Gender:               req.Gender,
+		Dob:                  req.DOB,
+		NewsletterSubscribed: req.SubscribeNewsletter,
+	})
+
+	if err != nil {
+		return nil, errors.New("Failed to initialize Profile!")
+	}
+
+	return &auth.AuthResult{
+		UserID:  newUserID,
+		Message: "Registration successful! Please log in.",
+	}, nil
+}
+
+func (uc *authUseCase) Login(ctx context.Context, email, password string) (*auth.AuthResult, error) {
+	return nil, nil
+}
+
+func (uc *authUseCase) LoginWithOTP(ctx context.Context, email, otp string) (*auth.AuthResult, error) {
+	return nil, nil
+}
+
+func (uc *authUseCase) SendOTP(ctx context.Context, email string) error {
+	return nil
+}
+
+func (uc *authUseCase) RegisterUser(ctx context.Context, req *auth.RegisterData) (*auth.AuthResult, error) {
+	return nil, nil
+}
+func (uc *authUseCase) Logout(ctx context.Context, token string) error {
+	return nil
+}
