1f17132ce2db9af79494f4a49a32c3dbd414906d
ShinyWise <franswinata06@gmail.com>
2026-06-01 14:00:26 +0700

    feat(backend): implement shared smtp mailer utility

---
+++ travelohi-backend/pkg/mailer/mailer.go
@@ -0,0 +1,58 @@
+package mailer
+
+import (
+	"fmt"
+	"net/smtp"
+	"strings"
+)
+
+type EmailSender interface {
+	SendEmail(to []string, subject string, htmlBody string) error
+}
+
+type smtpMailer struct {
+	host     string
+	port     string
+	username string
+	password string
+}
+
+func NewSMTPMailer(host, port, username, password string) EmailSender {
+	return &smtpMailer{
+		host:     host,
+		port:     port,
+		username: username,
+		password: password,
+	}
+}
+
+func (m *smtpMailer) SendEmail(to []string, subject string, htmlBody string) error {
+	from := "no-reply@travelohi.com"
+
+	// Construct MIME headers
+	header := make(map[string]string)
+	header["From"] = from
+	header["To"] = strings.Join(to, ",")
+	header["Subject"] = subject
+	header["MIME-version"] = "1.0"
+	header["Content-Type"] = "text/html; charset=\"UTF-8\""
+
+	// Combine headers
+	var message string
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + htmlBody
+
+	// Setup authentication
+	auth := smtp.PlainAuth("", m.username, m.password, m.host)
+	addr := fmt.Sprintf("%s:%s", m.host, m.port)
+
+	// Send email
+	err := smtp.SendMail(addr, auth, from, to, []byte(message))
+	if err != nil {
+		return fmt.Errorf("failed to send email: %w", err)
+	}
+
+	return nil
+}
